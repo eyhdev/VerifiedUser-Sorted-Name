f8d26be554ebf3c2f542f8b3ee7ccf074320bc6f
eyhdev <alaqsa335@gmail.com>
2023-08-13 14:05:32 +0300

    Initial Commit

---
+++ VerifiedUser/VerifiedUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifiedUserApp.swift
+//  VerifiedUser
+//
+//  Created by eyh.mac on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct VerifiedUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
